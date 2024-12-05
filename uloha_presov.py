9f2529e71d29a43604e005a4aa22ed5496dcecc1
Your Name <you@example.com>
2024-12-05 10:29:54 +0100

    vytvorenie funkcie dodavatelia

--- uloha_presov.py
+++ uloha_presov.py
@@ -6,8 +6,19 @@ def nacitaj_subor(nazov_suboru):
         return nacitany_subor
     
 
-
-
-
-
+def dodavatelia(data):
+    novy_slovnik = {}
+    for slovnik in data:
+        dodavatel = slovnik['Dodávateľ']
+        suma = slovnik['Celková_cena']
+        suma = suma.replace(',', '.')
+        suma = suma.replace(' ', '')
+        suma = float(suma)
+        if dodavatel in novy_slovnik:
+            novy_slovnik[dodavatel] += suma
+        else:
+            novy_slovnik[dodavatel] = suma
+    return novy_slovnik
+    
 data = nacitaj_subor('zoznam_faktury.json')
+print(dodavatelia(data))
